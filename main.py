332394a3123ef050be0fb5a5d67adb09dd2a42a1
Michael Joseph Miller <michael.joseph.millee@gmail.com>
2022-10-23 08:54:49 -0400

    many changes

--- main.py
+++ main.py
@@ -2,27 +2,18 @@
 """
 Starting point for Lottery Analyzer App
 """
-# import os
-
-# from dotenv import load_dotenv
-
-# import get_input as get
-
-# import FieldNumbers as fn
-# import lottery_analyzer_db as db
-# import lottery_data_api as api
-
-# load_dotenv()  # Get environment variables from .env file
 
 
 def main():
     """
     main function to run when this file is executed
     """
-    # db.updateDrawingsHistory(['powerball', 'mega_millions'])
-
-    import game
-    game = game.Game()
+    # Check last draw date in db
+    # get drawings since last in db
+    # format and insert into drawings db
+    # analyze drawings
+    # update analysis db
+    # display plots of data
 
 
 if __name__ == '__main__':
